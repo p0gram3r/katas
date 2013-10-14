0aba90c1d15b93d55d4977c1f675131b519f874d
p0gram3r <p0gram3r@gmail.com>
2013-10-14 22:47:04 +0200

    adds main method

--- src/main/java/codingdojo/HelloWorld.java
+++ src/main/java/codingdojo/HelloWorld.java
@@ -6,4 +6,8 @@ public class HelloWorld {
         return "Hello World!";
     }
 
+    public static void main(String[] args) {
+        System.out.println(new HelloWorld().getGreeting());
+    }
+
 }
