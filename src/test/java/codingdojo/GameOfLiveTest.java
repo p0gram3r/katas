1b63a7ae3a6d28d9eb2df7d26b1b224bc4ea1265
p0gram3r <p0gram3r@gmail.com>
2013-10-29 22:09:06 +0100

    refactored tests

--- src/test/java/codingdojo/GameOfLiveTest.java
+++ src/test/java/codingdojo/GameOfLiveTest.java
@@ -2,118 +2,83 @@ package codingdojo;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GameOfLiveTest {
 
+    /* @formatter:off */
+    private static final char[][] POPULATION = new char[][] {
+            {'.', '#', '.'},
+            {'.', '#', '.'},
+            {'.', '#', '.'}
+    };
+    /* @formatter:on */
+
+    private GameOfLife world;
+
+    @Before
+    public void setup() {
+        world = new GameOfLife(POPULATION);
+    }
+
     @Test
-    public void getNextGenerationForMiniWorldCorrectlyEvolvesNextGeneration() {
+    public void nextGenerationCorrectlyEvolvesNextPopulation() {
         /* @formatter:off */
-        char[][] generation1 = new char[][] {
-                {'#', '#'},
-                {'#', '.'}
-        };
-        char[][] generation2 = new char[][] {
-                {'#', '#'},
-                {'#', '#'}
+        char[][] expectedPopulation = new char[][] {
+            {'.', '.', '.'},
+            {'#', '#', '#'},
+            {'.', '.', '.'}
         };
         /* @formatter:on */
 
-        GameOfLife world = new GameOfLife(generation1);
-        assertArrayEquals(generation2, world.nextGeneration().getPopulation());
+        assertArrayEquals(expectedPopulation, world.nextGeneration().getPopulation());
     }
 
     @Test
     public void countLivingNeighborsReturnsTwoForTwoLivingNeighbors() {
-        /* @formatter:off */
-        char[][] generation1 = new char[][] {
-                {'.', '#', '.'},
-                {'.', '#', '.'},
-                {'.', '#', '.'}
-        };
-        /* @formatter:on */
-
-        GameOfLife world = new GameOfLife(generation1);
-        assertEquals(2, world.countLivingNeighbors(0, 0));
-        assertEquals(1, world.countLivingNeighbors(1, 0));
+        assertThat(world.countLivingNeighbors(0, 0), is(2));
     }
 
     @Test
     public void countLivingNeighborsReturnsOneForOneLivingNeighbor() {
-        /* @formatter:off */
-        char[][] generation1 = new char[][] {
-                {'.', '#', '.'},
-                {'.', '#', '.'},
-                {'.', '#', '.'}
-        };
-        /* @formatter:on */
-
-        GameOfLife world = new GameOfLife(generation1);
-        assertEquals(1, world.countLivingNeighbors(1, 0));
+        assertThat(world.countLivingNeighbors(1, 0), is(1));
     }
 
     @Test
     public void countLivingNeighborsReturnsThreeForThreeLivingNeighbors() {
-        /* @formatter:off */
-        char[][] generation1 = new char[][] {
-                {'.', '#', '.'},
-                {'.', '#', '.'},
-                {'.', '#', '.'}
-        };
-        /* @formatter:on */
-
-        GameOfLife world = new GameOfLife(generation1);
-        assertEquals(3, world.countLivingNeighbors(0, 1));
+        assertThat(world.countLivingNeighbors(0, 1), is(3));
     }
 
     @Test
     public void isAliveReturnsTrueForLivingCell() {
-        char[][] generation1 = new char[][] { { GameOfLife.LIVING_CELL } };
-
-        GameOfLife world = new GameOfLife(generation1);
-        assertThat(world.isAlive(0, 0), is(true));
+        assertThat(world.isAlive(1, 1), is(true));
     }
 
     @Test
     public void isAliveReturnsFalseForDeadCell() {
-        char[][] generation1 = new char[][] { { GameOfLife.DEAD_CELL } };
-
-        GameOfLife world = new GameOfLife(generation1);
         assertThat(world.isAlive(0, 0), is(false));
     }
 
     @Test
     public void isAliveReturnsFalseWhenXIsTooSmall() {
-        char[][] generation1 = new char[][] { { GameOfLife.LIVING_CELL } };
-
-        GameOfLife world = new GameOfLife(generation1);
         assertThat(world.isAlive(-1, 0), is(false));
     }
 
     @Test
     public void isAliveReturnsFalseWhenXIsTooBig() {
-        char[][] generation1 = new char[][] { { GameOfLife.LIVING_CELL } };
-
-        GameOfLife world = new GameOfLife(generation1);
-        assertThat(world.isAlive(1, 0), is(false));
+        assertThat(world.isAlive(3, 0), is(false));
     }
 
     @Test
     public void isAliveReturnsFalseWhenYIsTooSmall() {
-        char[][] generation1 = new char[][] { { GameOfLife.LIVING_CELL } };
-
-        GameOfLife world = new GameOfLife(generation1);
         assertThat(world.isAlive(0, -1), is(false));
     }
 
     @Test
     public void isAliveReturnsFalseWhenYIsTooBig() {
-        char[][] generation1 = new char[][] { { GameOfLife.LIVING_CELL } };
-
-        GameOfLife world = new GameOfLife(generation1);
-        assertThat(world.isAlive(0, 1), is(false));
+        assertThat(world.isAlive(0, 3), is(false));
     }
 }
