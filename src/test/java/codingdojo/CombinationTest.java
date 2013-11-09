5178f9b2ff62d06edbca7c07eb1ad0717c87d21c
p0gram3r <p0gram3r@gmail.com>
2013-11-09 19:21:32 +0100

    adds missing tests for 'equals'

--- src/test/java/codingdojo/CombinationTest.java
+++ src/test/java/codingdojo/CombinationTest.java
@@ -5,16 +5,53 @@ import static org.junit.Assert.assertThat;
 
 import java.awt.Color;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CombinationTest {
 
+    private Color[] colors;
+    private Combination combination;
+
+    @Before
+    public void before() {
+        colors = new Color[] { Color.RED, Color.YELLOW, Color.BLUE };
+        combination = new Combination(colors);
+    }
+
     @Test
     public void testGetColorsReturnsConstructorParameter() {
-        Color[] colors = new Color[] { Color.RED, Color.YELLOW, Color.BLUE };
 
-        Combination c = new Combination(colors);
-        assertThat(c.getColors(), is(colors));
+        assertThat(combination.getColors(), is(colors));
+    }
+
+    @Test
+    public void testEqualsReturnsTrueForSelf() {
+        assertThat(combination.equals(combination), is(true));
+    }
+
+    @Test
+    public void testEqualsReturnsTrueForCombinationWithEqualColorArray() {
+        Combination other = new Combination(colors);
+
+        assertThat(combination.equals(other), is(true));
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForNull() {
+        assertThat(combination.equals(null), is(false));
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForUnequalColorArray() {
+        Combination other = new Combination(new Color[0]);
+
+        assertThat(combination.equals(other), is(false));
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForIncompatibleObject() {
+        assertThat(combination.equals("incompatible"), is(false));
     }
 
 }
