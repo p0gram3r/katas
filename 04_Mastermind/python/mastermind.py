3aa4ad4bada6f95eb59850731c199aa0988e2a6a
p0gram3r <p0gram3r@gmail.com>
2013-11-14 18:38:08 +0100

    skeleton UI

--- 04_Mastermind/python/mastermind.py
+++ 04_Mastermind/python/mastermind.py
@@ -1,1 +1,20 @@
+import simplegui
 
+
+
+def new_game():
+    print "Good job!"
+
+
+    
+def draw(canvas):
+    pass
+    
+
+frame = simplegui.create_frame("Mastermind", 400, 600)
+frame.add_button("new game", new_game)
+frame.set_draw_handler(draw)
+
+# start the show
+new_game()
+frame.start()
